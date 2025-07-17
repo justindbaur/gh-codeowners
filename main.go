a64881b370bd586917debcb0f3bea7d69138d3bc
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2025-07-17 10:16:00 +0000

    Command cleanup

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
 	"maps"
 	"os"
@@ -101,15 +102,9 @@ func main() {
 
 	// stage command
 	cmd.AddCommand(&cobra.Command{
-		Use: "stage",
+		Use: "stage team",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// Expect a third argument for the team you want to stage for
-			if len(os.Args) != 3 {
-				fmt.Println("Expected a third argument to be the team you want to stage files for.")
-				// TODO: Return error
-				return nil
-			}
-
+			// TODO: Get team arg from cmd
 			team := os.Args[2]
 
 			foundFileToStage := false
@@ -120,13 +115,13 @@ func main() {
 					foundFileToStage = true
 					err := exec.Command(gitBin, "add", edittedFilesScanner.Text()).Run()
 					if err != nil {
-						fmt.Printf("Failed to stage '%s'\n", edittedFilesScanner.Text())
+						return fmt.Errorf("Failed to stage '%s'\n", edittedFilesScanner.Text())
 					}
 				}
 			}
 
 			if !foundFileToStage {
-				fmt.Printf("Did not find any files owned by '%s' run gh codeowners report to see who all owns file in your editted files.\n", team)
+				return fmt.Errorf("Did not find any files owned by '%s' run gh codeowners report to see who all owns file in your editted files.\n", team)
 			}
 
 			return nil
@@ -173,9 +168,7 @@ func main() {
 				optionIndex, err := p.Select(fmt.Sprintf("Choose where to put %d unowned files", len(unownedFiles)), "", options)
 
 				if err != nil {
-					fmt.Println(err)
-					// TODO: Return actual error
-					return nil
+					return fmt.Errorf("error requesting what to do with unowned files: %v", err)
 				}
 
 				option := options[optionIndex]
@@ -193,47 +186,35 @@ func main() {
 
 			if len(filesMap) == 0 {
 				// Nothing to do, stop here
-				fmt.Println("There are no files to make PR's for.")
-				// TODO: Return actual error
-				return nil
+				return fmt.Errorf("there are no files to make PR's for")
 			}
 
 			branchTemplate, err := p.Input("Enter branch template", "")
 
 			if err != nil {
-				fmt.Println(err)
-				// TODO: Return actual error
-				return nil
+				return fmt.Errorf("error while requesting branch template: %v", err)
 			}
 
 			if !strings.Contains(branchTemplate, "{team}") {
-				fmt.Println("Branch template does not contain '{team}'")
-				// TODO: Return actual error
-				return nil
+				return fmt.Errorf("branch template does not contain '{team}'")
 			}
 
 			prTemplateFile, err := p.Input("Enter the path to the file containing your PR template", "./.github/PULL_REQUEST_TEMPLATE.md")
 
 			if err != nil {
-				fmt.Println(err)
-				// TODO: Return actual error
-				return nil
+				return fmt.Errorf("error while requesting path to PR template: %v", err)
 			}
 
 			commitMessageTemplate, err := p.Input("Enter the commit message you want to use", "")
 
 			if err != nil {
-				fmt.Println(err)
-				// TODO: Return actual error
-				return nil
+				return fmt.Errorf("Error while getting commit message template: %v", err)
 			}
 
 			prTemplateFileContents, err := os.ReadFile(prTemplateFile)
 
 			if err != nil {
-				fmt.Println(err)
-				// TODO: Return actual error
-				return nil
+				return fmt.Errorf("Error while reading PR template file: %v", err)
 			}
 
 			prompt := &survey.Editor{
@@ -249,9 +230,7 @@ func main() {
 			err = survey.AskOne(prompt, &contents)
 
 			if err != nil {
-				fmt.Println(err)
-				// TODO: Return actual error
-				return nil
+				return fmt.Errorf("error while requesting PR template edit: %v", err)
 			}
 
 			fmt.Println("Finished prompting the user.")
@@ -274,9 +253,7 @@ func main() {
 				displayName, err := p.Input("Choose display name for team '"+team+"'", team)
 
 				if err != nil {
-					fmt.Println(err)
-					// TODO: Return actual error
-					return nil
+					return fmt.Errorf("error requesting display name for %s: %v", team, err)
 				}
 
 				teamBranch := strings.ReplaceAll(branchTemplate, "{team}", displayName)
@@ -377,11 +354,7 @@ func main() {
 				stdOut, stdErr, err := gh.Exec(args...)
 
 				if err != nil {
-					fmt.Println("Error creating PR with GitHub CLI")
-					fmt.Println(err)
-					os.Stderr.Write(stdErr.Bytes())
-					// TODO: Return actual error
-					return nil
+					return fmt.Errorf("Error creating PR with GitHub CLI: %v", err)
 				}
 
 				// Should we do anything else here?
@@ -392,11 +365,7 @@ func main() {
 				checkoutOutput, err = exec.Command(gitBin, "checkout", "-").Output()
 
 				if err != nil {
-					fmt.Println("Error trying to checkout base branch")
-					fmt.Println(err)
-					os.Stdout.Write(checkoutOutput)
-					// TODO: Return actual error
-					return nil
+					return fmt.Errorf("Error trying to checkout base branch: %v", err)
 				}
 
 				os.Stdout.Write(checkoutOutput)
@@ -430,4 +399,54 @@ func getCodeownersInfo() (*Codeowners, error) {
 	}
 
 	return nil, fmt.Errorf("could not locate a CODEOWNERS file")
+}
+
+func GetCodeowners(cmd *cobra.Command) (*Codeowners, error) {
+	return nil, errors.New("not implemented")
+}
+
+type AutoPROptions struct {
+	IsDraft        bool
+	CommitTemplate string
+	BranchTemplate string
+	// TODO: Allow body in non-interactive way
+
+	UnownedFiles string
+	DryRun       bool
+}
+
+func newAutoPullRequestCmd() *cobra.Command {
+	opts := &AutoPROptions{}
+
+	cmd := &cobra.Command{}
+
+	fl := cmd.Flags()
+	fl.BoolVarP(&opts.IsDraft, "draft", "d", false, "Mark the pull requests as drafts")
+	fl.StringVarP(&opts.CommitTemplate, "commit", "c", "", "The template string to use for each commit")
+	fl.StringVarP(&opts.BranchTemplate, "branch", "b", "", "The template string to use for each branch that is created")
+	fl.StringVarP(&opts.UnownedFiles, "unowned-files", "u", "", "What PR to put unowned files onto. `seperate` to make their own PR.")
+	fl.BoolVar(&opts.DryRun, "dry-run", false, "Print details instead of creating the PR. May still push git changes.")
+
+	_ = cmd.RegisterFlagCompletionFunc("unowned-files", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		// TODO: Do early parse of CODEOWNERS file to help fill in option
+		return nil, cobra.ShellCompDirectiveError
+	})
+
+	return cmd
+}
+
+type FormatOptions struct {
+	Team      string
+	CanPrompt bool
+	Global    map[string]string
+	Scoped    map[string]string
+}
+
+func (formatOptions *FormatOptions) NewScope(team string) *FormatOptions {
+	return &FormatOptions{
+		Team:      team,
+		CanPrompt: formatOptions.CanPrompt,
+		Global:    formatOptions.Global,
+		Scoped:    map[string]string{},
+	}
 }
