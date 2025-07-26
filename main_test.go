cf7508622b480c56b63821973993bdf19b3b1434
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2025-07-26 08:05:19 -0400

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -22,7 +22,7 @@ func (p *mockPrompter) Select(prompt, defaultValue string, options []string) (in
 	return args.Int(0), args.Error(1)
 }
 
-func (p *mock Prompter) Confirm(prompt string, defaultValue bool) (bool, error) {
+func (p *mockPrompter) Confirm(prompt string, defaultValue bool) (bool, error) {
 	args := p.Called(prompt, defaultValue)
 	return args.Bool(0), args.Error(1)
 }
