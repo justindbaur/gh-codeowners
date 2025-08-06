2c50cccc11fe2e8b288cac07b6907637b9d264ec
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2025-08-06 21:35:25 +0000

    Add team parsing utilities

--- cmd/shared.go
+++ cmd/shared.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/justindbaur/gh-codeowners/codeowners"
 	"github.com/spf13/cobra"
@@ -47,4 +49,31 @@ func AddOrUpdate[K comparable, E any](m map[K]E, key K, initialValue E, updater 
 	} else {
 		m[key] = initialValue
 	}
+}
+
+type Team struct {
+	org  string
+	name string
+}
+
+func ParseTeam(value string) (*Team, error) {
+	if !(strings.Index(value, "@") == 0) {
+		return nil, errors.New("missing @ at the beginning")
+	}
+
+	split := strings.Split(value[1:], "/")
+
+	if len(split) != 2 {
+		return nil, errors.New("cannot be split in two by /")
+	}
+
+	return &Team{org: split[0], name: split[1]}, nil
+}
+
+func (t *Team) ShortName() string {
+	return t.name
+}
+
+func (t *Team) FullName() string {
+	return fmt.Sprintf("@%s/%s", t.org, t.name)
 }
